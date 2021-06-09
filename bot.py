647caf618c5c29c13c853bef2b9982034d5eabd4
BMO-Noire <anji0903@naver.com>
2021-06-09 19:33:33 +0900

    텔레그램 봇 완성

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from pprint import pprint
 import time
 import requests
 from bs4 import BeautifulSoup
+import random
+import string
 
 
 TOKEN = '1892597592:AAGD-VUiqCC8CxD5mIfJSkkXrV0X3zCXglk'
@@ -33,14 +35,39 @@ def handle(msg):
         result = result.replace("+", "\n2등 보너스: ")
         result = result.replace(". ", ".\n")
         result = result.replace(", 1인당 당첨금액 ", ", 각 ")
+
         if ',,,,,' in result:
             result = str(args[1]) + '회차는 아직 확인되지 않았습니다.'
             return False, sendMessage(chat_id, result)
+        if '0,0,0,0,0,0' in result:
+            return False, sendMessage(chat_id, '숫자로만 입력해주세요')
+
         sendMessage(chat_id, result)
         print(numberData)
+    elif text.startswith('복권생성') and len(args)>1:
+        num = str(args[1])
+        if(num.isdigit() == False):
+            return False, sendMessage(chat_id, '숫자로만 입력해주세요')
+        
+        res = '\n'
+        if int(args[1]) > 5:
+            return False, sendMessage(chat_id, '복권번호는 최대 5개까지 생성 가능합니다')
+
+        for i in range(0, int(args[1])):
+            lotto = random.sample(range(1, 46), 6)
+            lotto.sort()
+            res += str(lotto) + '\n'
+            print(res)
+
+        res2 = '복권 번호를 생성합니다\n' + res + '\n'+str(args[1]) + '개 생성 완료!'
+        print(res)
+        sendMessage(chat_id, res2)
+
+    elif text.startswith('명령어'):
+        sendMessage(chat_id, '회차 [회차번호]\n복권생성 [개수(최대 5)]')
 
     else:
-        sendMessage(chat_id, '모르는 명령어입니다.\n지역 [지역번호], 저장 [지역번호], 확인 중 하나의 명령을 입력하세요.')
+        sendMessage(chat_id, '모르는 명령어입니다.\n회차 [회차번호], 번호생성 [개수(최대 5)], 확인 중 하나의 명령을 입력하세요.')
 
 
 def sendMessage(user, msg):
